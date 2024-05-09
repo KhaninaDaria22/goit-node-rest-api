8af559b0fd4c5679c00c89be5fc6572da1a59616
Daria Khanina <khanina_dasha@ukr.net>
2024-05-09 23:27:44 +0300

    commit 4

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 import mongoose from "mongoose";
 import 'dotenv/config'
+import {app} from "./app.js"
 
 
+const {DB_URL, PORT = 3000} = process.env; 
 
-const DB_URL = process.env.DB_URL 
 mongoose
   .connect(DB_URL)
-  .then(() => console.log("Database connection successfully"))
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Database connection successful");
+    });
+  })
   .catch((error) => {
     console.error(error);
     process.exit(1);
